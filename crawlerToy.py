10fb8e94f4463c66cea9473c4e76f01a04054be6
wjl-simon <simon30302@hotmail.com>
2021-07-12 09:09:51 +0800

    merging

--- crawlerToy.py
+++ crawlerToy.py
@@ -11,5 +11,5 @@ uri = baseUri + '/ws/' + symbol + '@kline_' + interval
 def on_message(wsapp, message):
     print(message)
 
-wsapp = websocket.WebSocketApp(uri, on_message=on_message)
+wsapp = websocket.WebSocketApp(uri, on_message = on_message)
 wsapp.run_forever()
