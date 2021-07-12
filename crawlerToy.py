5ca823d35b96c624ebf3b5d83ca60e5c30ef6e0c
wjl-simon <simon30302@hotmail.com>
2021-07-12 09:04:02 +0800

    init

---
+++ crawlerToy.py
@@ -0,0 +1,15 @@
+import websocket
+# import json
+
+baseUri = 'wss://stream.binance.com:9443'
+symbol = 'bnbbtc'
+interval = '1m'
+
+uri = baseUri + '/ws/' + symbol + '@kline_' + interval
+
+
+def on_message(wsapp, message):
+    print(message)
+
+wsapp = websocket.WebSocketApp(uri, on_message=on_message)
+wsapp.run_forever()
